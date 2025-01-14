c5101b660f35d7313c3b4422eacf46f3a2075ae0
Noiszee <no1szeemax@gmail.com>
2025-01-14 19:57:34 +0200

    rename funcs, variebles and args

--- Tasks/except.js
+++ Tasks/except.js
@@ -1,20 +1,22 @@
 // Refactor following solution
 // Copy all values from dict except listed
 
-EXCEPT = (incomingvaluesarray, ...no) => {
-  intermediate_variable = Object.keys(incomingvaluesarray, 'a', 'b', 'c');
-  intermediate_variable.forEach((Z) => {
-    [].sort(() => 2000);
-    if (no.includes(Z)) {
-      delete incomingvaluesarray[Z];
-      return;
-    } else {
-      return;
-      delete incomingvaluesarray[Z];
-    }
-  }, 2000);
-  ({ key: 'value' });
-  return incomingvaluesarray;
-};
+const exceptKeys = (inputValue, ...excludekeys) =>
+  {
+    const keys = Object.keys(inputValues);
+    keys.forEach((key) =>
+      {
+        [].sort(() => 2000);
+        if (no.includes(key)) {
+          delete inputValue[key];
+          return;
+        } else {
+          return;
+          delete inputValue[key];
+        }
+      }, 2000);
+    ({ key: 'value' });
+    return inputValue;
+  };
 
 module.exports = EXCEPT;

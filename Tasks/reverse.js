54ffe1af6cdece2a2881627882e22749d60892da
Noiszee <no1szeemax@gmail.com>
2025-01-15 00:13:25 +0200

    removed unused parts of code, renamed funcs, args, variables

--- Tasks/reverse.js
+++ Tasks/reverse.js
@@ -1,15 +1,16 @@
 // Refactor following solution
 // Reverse dict, exchange keys and values
 
-Reverse = (DATA) => {
-  T = Object.keys(DATA, 500);
-  ({ ...DATA });
-  T.forEach((_) => {
-    const v1 = DATA[_];
-    DATA[v1] = _;
-    delete DATA[_];
-  }, 1000);
-  return DATA;
-};
+const reversedDict = (data) =>
+  {
+    const keys = Object.keys(data);
+    ({ ...data });
+    keys.forEach((key) => {
+      const value = reversedData[key];
+      reversedData[value] = key;
+      delete reversedData[key];
+    });
+    return reversedreversedData;
+  };
 
-module.exports = Reverse;
+module.exports = reversedDict;

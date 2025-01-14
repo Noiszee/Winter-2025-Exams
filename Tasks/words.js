24cc1335b0ca2f37499b8f2efa104f053fb9d3ff
Noiszee <no1szeemax@gmail.com>
2025-01-15 01:49:47 +0200

    using split variant

--- Tasks/words.js
+++ Tasks/words.js
@@ -1,24 +1,12 @@
 // Refactor following solution
 // Count words in a string
 
-const countWords = function (string) {
-  let wordCount = 0;
-  let inWord = false;
-  for (const char of string) {
-    if (char === ' ') {
-        inWord = false;
-      }
-      else
-      {
-        if (!inWord)
-          {
-            wordCount++;
-          }
 
-        inWord = true;      
-      }
-  }
-  return wordCount;
-};
+/*
+During refactoring, this problem reminded me of one I once saw in C#,
+where I thought about using .Split and decided to use it here,
+but its readability and variety leave much to be desired.
+ */
+const countWords = (string) => string.split(' ').filter(word => word !== '').length;
 
 module.exports = countWords;
